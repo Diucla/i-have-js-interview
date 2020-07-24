918c178753858d4a47fa0eb4340b5b2d8308f317
Claudio Sumburane <claudiosumburane@gmail.com>
2020-07-24 23:23:06 +0200

    Update complexity.js
    reduce complexity

--- algorithms/complexity.js
+++ algorithms/complexity.js
@@ -19,5 +19,24 @@ const isUnique = (arr) => {
     return result;
 };
 
-// The above result is correct but the time complexity is n^2, so how can we improve? 
-// we can use something called memoization, in the next commit i'll add definition in readme file
+// The above result is correct but the time complexity is O(n^2), so how can we improve? 
+
+const isUniqueMemoize = (arr) => {
+    // we are create a object that will hold all the uniques values
+    const breadcrumbs = {};
+    let result = true;
+
+    for (let i = 0; i < arr.length; i++)
+        // check if the current value exist in the unique object or not
+        // if yes return false the array is not unique
+        // if not add to the unique object
+        if (breadcrumbs[arr[i]]) {
+            result = false;
+            break;
+        } else {
+            breadcrumbs[arr[i]] = true;
+        }
+
+    return result;
+};
+// Complexity of `isUniqueMemoize` function above is O(n)
