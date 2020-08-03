b13fa6957dc7dcc8cf06e5eedbc0e90499810646
Claudio Sumburane <claudiosumburane@gmail.com>
2020-08-03 21:51:42 +0200

    implement removeNode and removeEdge

--- graph/graph-map.js
+++ graph/graph-map.js
@@ -28,4 +28,27 @@ class Graph {
         this.adjacencyList.get(origin).push(destination);
         this.adjacencyList.get(destination).push(origin);
     }
+
+    removeNode(node) {
+        // get the neighbors
+        let neighbors = this.adjacencyList.get(node);
+
+        // loop and remove all edges between the "removed" node and their neighbors
+        for (let neighbor of neighbors) {
+            this.removeEdge(node, neighbor)
+        }
+
+        // delete the node
+        delete this.adjacencyList.get(node);
+    }
+
+    removeEdge(origin, destination) {
+        // get the origin, array of values, and returns elements different of the destination node 
+        this.adjacencyList.get(origin) = this.adjacencyList.get(origin)
+            .filter(element => element !== destination);
+
+        // get the destination, array of values, and returns elements different of the origin node 
+        this.adjacencyList.get(destination) = this.adjacencyList.get(destination)
+            .filter(element => element !== origin);
+    }
 }
--- graph/graph-obj.js
+++ graph/graph-obj.js
@@ -28,4 +28,27 @@ class Graph {
         this.adjacencyList[origin].push(destination);
         this.adjacencyList[destination].push(origin);
     }
+
+    removeNode(node) {
+        // get the neighbors
+        let neighbors = this.adjacencyList[node];
+
+        // loop and remove all edges between the "removed" node and their neighbors
+        for (let neighbor of neighbors) {
+            this.removeEdge(node, neighbor)
+        }
+
+        // delete the node
+        delete this.adjacencyList[node];
+    }
+
+    removeEdge(origin, destination) {
+        // get the origin, array of values, and returns elements different of the destination node 
+        this.adjacencyList[origin] = this.adjacencyList[origin]
+            .filter(element => element !== destination);
+
+        // get the destination, array of values, and returns elements different of the origin node 
+        this.adjacencyList[destination] = this.adjacencyList[destination]
+            .filter(element => element !== origin);
+    }
 }
